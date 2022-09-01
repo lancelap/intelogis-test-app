8ec2aab17eb18137221ccb49b990c006a176aad8
Daniel Potapov <lancelap@mail.ru>
2022-09-01 20:01:26 +0300

    Add separator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { DatePicker } from 'antd';
+import { Allotment } from "allotment";
+import "allotment/dist/style.css";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Route Map Application
+      <Allotment>
+        <Allotment.Pane minSize={500}>
+          <div>ComponentA</div>
+        </Allotment.Pane>
+        <Allotment.Pane minSize={500}>
+          <div>ComponentB</div>
+        </Allotment.Pane>
+      </Allotment>
     </div>
   );
 }
